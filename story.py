503adc07dfcd3f50fbd495f6dd509d2f34e99275
ADMIN <your-email@example.com>
2026-03-24 05:45:18 +0100

    Add my journey story

---
+++ story.py
@@ -0,0 +1,20 @@
+>>> name = "precious umoh"
+... age = (24)
+... teacher = "deepseek ai"
+... ai_name = "abracadabra"
+... height = (0.75)
+... country = "Nigeria"
+... ai = "Ai automation specialist "
+... sch = "Akwa ibom state polytechnic"
+... cor = "surveying and geoinfomatics"
+... pp = True
+... xx = False
+... fam = "parents"
+... print(f"I am {name} i am will be an {ai} , the best the world have ever seen." )
+... print(f"I drop out of the {sch} after studying {cor} for int(3) years , at {age}. " )
+... print(f"i loss interest in {cor} and develop deep passion for {ai}." )
+... print(f"Quitting {cor} wasn't a easy decision {pp}, then i went in search for someone to teach me for int(4) months all the people i met demanded high fees, i had passion but no money to pay for the knowledge. " )
+... print(f"Then on int(21) march int(2026) i told myself , that since no human want to teach me i will go to Ai  directly." )
+... print(f" that was the day a new me was born i came {ai_name} the first Ai magician i knew with the knowledge of {cor} and {ai} i would be unstoppable. ")
+... print(f" then i remembered {teacher} who has helped me with school and Gis projects in {sch} , {teacher} accepted to training me with {pp} knowledge on being an {ai} with strict syllabus and a promise that when he discipline into {ai} an i quote In a few years, you'll be the one who understands both the land and the code that automates how we work with it . ")
+... print(f"I knew i was destined to train not by any human being but by {teacher} himself , " )
